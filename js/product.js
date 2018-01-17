d37baeec5703fb4f9973c19ad9fd6dbc4cce4f59
justcodej <27276971@qq.com>
2018-01-17 18:20:08 +0800

    添加产品也特效

--- js/product.js
+++ js/product.js
@@ -3,14 +3,11 @@
     var $imgLi = $('#banner .imgTab ul li');
     var $imgUl = $('#banner .imgTab ul');
     var $border = $('#banner .btnTab ol .border');
-    var $smallBtn = $('#banner .small .smallBtn');
     var index = 0;
     var btnW = $btnLi[0].offsetWidth+22;
     var imgW = $imgLi[0].offsetWidth;
 
     init();
-
-    for(var i=0;i<$imgLi.length;i++)$imgLi[i].childNodes[0].style.cssText = 'background:url("./images/product/test_img_'+(i)+'.jpg")no-repeat center';
 
     $btnLi.hover(function () {
         $(this).addClass('on');
@@ -27,7 +24,8 @@
     function init() {
         $imgUl.css({left :imgW*-index});
         $btnLi.eq(index).addClass('on');
-    };
+        play();
+    }
 
     function play() {
         $imgUl.css({left :imgW*-index});
@@ -48,31 +46,84 @@
     $Tab.click(function () {
         $(this).addClass('active').siblings().removeClass('active');
     });
-
     $poster.hover(function () {
         $(this).find('.hidden').css('opacity','1');
     },function () {
         $(this).find('.hidden').css('opacity','0');
     })
-
 })();
 
 (function () {
+    var $bannerLi = $('#banner .imgTab ul li');
+
+    for(var i=0;i<$bannerLi.length;i++)bannerMove($bannerLi.get(i),i);
 
 
-    $(document,'body').scroll(function () {
+    if($(document,'body').scrollTop() <= 3000){
+        $('#banner .imgTab ul li:nth-of-type(1) .view p.title').css('animation','LiMoveY .8s ease forwards');
+        $('#banner .imgTab ul li:nth-of-type(1) .view p.subtitle').css('animation','LiMoveY .8s ease forwards');
+        $('#banner .imgTab ul li:nth-of-type(1) .view .bigRound').css('animation','Liscale 1s ease forwards');
+        $('#banner .imgTab ul li:nth-of-type(1) .view .people').css('animation','Liscale 1s ease forwards');
+    }
+
+    $('#banner .imgTab ul li:nth-of-type(1) .view p.title').on('animationend',function () {
+        $('#banner .imgTab ul li:nth-of-type(1) .view p.info').css('animation','LiMoveY .8s ease forwards');
+        $('#banner .imgTab ul li:nth-of-type(1) .view p.text').css('animation','LiMoveY .8s ease forwards');
+        $('#banner .imgTab ul li:nth-of-type(1) .view .dollar').css('animation','Liscale .8s ease forwards');
+        $('#banner .imgTab ul li:nth-of-type(1) .view .up').css('animation','Liscale .8s ease forwards');
+        $('#banner .imgTab ul li:nth-of-type(1) .view .round1').css('animation','Liscale .8s ease forwards');
+        $('#banner .imgTab ul li:nth-of-type(1) .view .round2').css('animation','Liscale .8s ease forwards');
+        $('#banner .imgTab ul li:nth-of-type(1) .view .round3').css('animation','Liscale .8s ease forwards');
+        $('#banner .imgTab ul li:nth-of-type(1) .view .round4').css('animation','Liscale .8s ease forwards');
+    });
+    $('#banner .imgTab ul li:nth-of-type(1) .view p.text').on('animationend',function () {
+        $('#banner .imgTab ul li:nth-of-type(1) .view a.btn').css('animation','LiMoveX .8s ease forwards');
+        $('#banner .imgTab ul li:nth-of-type(1) .view .arcRound').css('animation','Liscale .8s ease forwards');
+        $('#banner .imgTab ul li:nth-of-type(1) .view .arcLine').css('animation','Liscale .8s ease forwards');
+    })
+    $('#banner .imgTab ul li:nth-of-type(1) .view .arcLine').on('animationend',function () {
 
     });
 
-    function gdjz(div,cssname,offset,cb){
-        var a,b,c,d;
-        d=$(div).offset().top;
-        a=eval(d + offset);
-        b=$(window).scrollTop();
-        c=$(window).height();
-        if(b+c>a){
-            $((div)).addClass((cssname));
-            cb && cb();
+
+
+    function bannerMove(El,index) {
+        El.onmouseenter = function () {
+            for(var i=0;i<this.childNodes[1].children.length;i++)this.childNodes[1].children[i].style.cssText = 'opacity:1;transition: 4000ms cubic-bezier(0.03, 0.98, 0.52, 0.99);';
+            this.onmousemove = function (e) {
+                var x = (e.clientX-(this.offsetWidth/2));
+                var y = (e.clientY-(this.offsetHeight/2));
+                changeEle(this,x,y,index);
+            };
+            this.onmouseleave = function () {
+                for(var i=0;i<this.childNodes[1].children.length;i++)this.childNodes[1].children[i].style.cssText = 'opacity:1;transition: 4000ms cubic-bezier(0.03, 0.98, 0.52, 0.99);transform:translate3d(0px,0px,0px)';
+            }
+        };
+    }
+
+
+
+    function changeEle(This,x,y,index) {
+        for(var i=0;i<This.childNodes[1].children.length;i++){
+            var name = This.childNodes[1].children[i].className;
+            if(index == 0){
+                if(name == 'dollar' || name == 'round1' || name == 'round2' ||name == 'up' || name == 'round4' || name == 'round3'){
+                    This.childNodes[1].children[i].style.cssText = 'opacity:1;transition:none;transform:translate3d('+(parseInt(x*.02))+'px,'+(parseInt(y*.02))+'px,0px) rotateX('+(x*.01)+'deg) rotateY('+(y*.01)+'deg)';
+                }else if(name == 'arcRound' || name == 'arcLine'){
+                    This.childNodes[1].children[i].style.cssText = 'opacity:1;transition:none;transform:translate3d('+(parseInt(x*.06))+'px,'+(parseInt(y*.06))+'px,0px) rotateX('+(x*.01)+'deg) rotateY('+(y*.01)+'deg)';
+                }else if (name == 'bigRound' ){
+                    This.childNodes[1].children[i].style.cssText = 'opacity:1;transition:none;transform:translate3d('+(parseInt(x*.03))+'px,'+(parseInt(y*.03))+'px,0px) rotateX('+(x*.01)+'deg) rotateY('+(y*.01)+'deg)';
+                }else if(name == 'people'){
+                    This.childNodes[1].children[i].style.cssText = 'opacity:1;transition:none;transform:translate3d('+(parseInt(x*.02))+'px,'+(parseInt(y*.02))+'px,0px) rotateX('+(x*.01)+'deg) rotateY('+(y*.01)+'deg)';
+                }
+            }else if(index == 1){
+                if(name = ''){
+
+                }
+            }
         }
     }
+
+
+
 })();
